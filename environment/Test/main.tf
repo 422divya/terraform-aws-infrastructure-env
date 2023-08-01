a9a12c33740d63f38dfaa7c822b469e09d0237af
Divya Prabhu <85998740+422divya@users.noreply.github.com>
2023-08-01 16:13:23 +0530

    Create main.tf

---
+++ environment/Test/main.tf
@@ -0,0 +1,26 @@
+terraform {
+   required_providers {
+     aws = {
+         source = "hashicorp/aws"
+         version = "5.1.0"
+
+}
+
+}
+
+
+
+backend "s3" {
+
+ bucket = "bucket-terraform-1"
+ key = "Test-env/Teststate.tf"
+ region = "ap-northeast-1"
+ dynamodb_table = "state-db"
+
+}
+
+}
+
+provider "aws" {
+ region = "ap-northeast-1"
+}
