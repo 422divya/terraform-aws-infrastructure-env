ccd9a0bdb5df894c97d4042c041c59ba8d9e39c2
Divya Prabhu <85998740+422divya@users.noreply.github.com>
2023-08-01 16:14:01 +0530

    Create variable.tf

---
+++ environment/Test/variable.tf
@@ -0,0 +1,21 @@
+variable "server" {
+  description = "Name to be used on ec2 instance"
+  type = string
+  default = "Test-server"
+}
+
+
+variable "instance-type" {
+
+description = "instance type to be used"
+type = string
+default = "t2.micro"
+
+}
+
+
+variable "amiid" {
+type = string
+default = "ami-08c84d37db8aafe00"
+
+}
